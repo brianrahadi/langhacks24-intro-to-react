ddfda3a8c40ff921c9eab02c263085b3b9f6e246
Brian Rahadi <brian.rahadi@gmail.com>
2024-09-18 01:53:10 -0700

    add message

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import './Default.css'
-
 import Header from './components/Header'
 import HeroSection from './components/HeroSection'
 import AboutSection from './components/AboutSection'
